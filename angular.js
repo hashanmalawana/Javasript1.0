138b545007faa42f75cddadb83dd61ee2fd1d282
Hashan Malawana <hashanmalawana@gmail.com>
2018-03-09 23:42:30 +0530

    Add files via upload

---
+++ angular.js
@@ -0,0 +1,12 @@
+
+<html ng-app="">
+<head>
+	<title>Angular P1</title>
+	<script type="text/javascript"></script>
+</head>
+
+<body>
+<input type="text" ng-model="name">
+<h2>Hello {{name}}</h2>
+</body>
+</html>
